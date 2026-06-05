977e56441e90b5abcde45d71244c039bc16b070f
prajwal-abcdesigns <prajwal@abcdesigns.in>
2026-06-05 15:31:30 +0530

    Updated prisma.config.ts  file for production build -> error came on vercel

--- app/(dashboard)/dashboard/authors/[id]/edit/page.tsx
+++ app/(dashboard)/dashboard/authors/[id]/edit/page.tsx
@@ -21,11 +21,8 @@ export default async function EditAuthorPage({ params }: { params: Promise<{ id:
             initialData={{
                 name: author.name,
                 email: author.email,
-                // Property 'email' does not exist on type '{ id: number; name: string; profileImage: string | null; }'.
                 bio: author.bio || "",
-                // Property 'bio' does not exist on type '{ id: number; name: string; profileImage: string | null; }'.
                 avatar: author.avatar || "",
-                // Property 'avatar' does not exist on type '{ id: number; name: string; profileImage: string | null; }'.
             }}
         />
     );
