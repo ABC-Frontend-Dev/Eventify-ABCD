1961e07ade5cae592b083bc431af01e2685c91eb
prajwal-abcdesigns <prajwal@abcdesigns.in>
2026-06-09 16:54:36 +0530

    Updated the blog layout and made it responsive

--- app/(frontend)/blog/page.tsx
+++ app/(frontend)/blog/page.tsx
@@ -6,25 +6,27 @@ import Image from "next/image";
 
 export default function Blog() {
     return (
-        <div className="max-w-360 w-full mx-auto px-20 py-9">
+        <div className="max-w-360 w-full mx-auto px-5 lg:px-20 py-9">
             <Suspense fallback={<BlogListHeaderSkeleton />}>
                 <BlogListHeader />
             </Suspense>
 
             <div className="mt-7.5">
-                <div className="flex items-start justify-center gap-5">
-                    <div className="max-w-157.5 w-full h-full relative">
-                        <figure className="h-142.25 w-full overflow-hidden relative after:absolute after:w-full after:h-full after:inset-0 after:bg-black/20 after:pointer-events-none">
+                <div className="flex items-start justify-center flex-col lg:flex-row gap-5">
+                    <div className="max-w-full lg:max-w-157.5 w-full h-full relative">
+                        <div className="absolute top-3 lg:top-5 right-3 lg:right-5 z-40 border border-primary/80 bg-primary/80 rounded-[6px] p-2 lg:p-2.5 capitalize text-xs lg:text-sm font-product-sans-medium font-light w-fit text-white">
+                            Activations
+                        </div>
+                        <figure className="h-68.5 lg:h-142.25 w-full overflow-hidden relative after:absolute after:w-full after:h-full after:inset-0 after:bg-black/20 after:pointer-events-none">
                             <Image src="/images/blogs/Group 48529.png" alt="blog1" width={1000} height={1000} className="h-full w-full object-cover" />
                         </figure>
-                        <div className="absolute w-[94%] bottom-3.5 left-1/2 -translate-x-1/2 p-5 bg-black/20 backdrop-blur-lg z-10">
-                            <div className="border border-primary/80 bg-primary/80 rounded-[6px] p-2.5 capitalize font-product-sans-medium font-light w-fit text-white">Activations</div>
-                            <div className="mt-1.75 text-[20px] leading-6.5 tracking-wide font-product-sans-bold font-extralight text-white">
+                        <div className="absolute w-full bottom-0 left-0 p-5 blog-page-gradient z-10">
+                            <div className="text-sm lg:text-[20px] leading-4.5 lg:leading-6.5 tracking-wide font-product-sans-bold font-extralight text-white">
                                 Emirates College for Advanced Education concludes 10th Annual Autism Day event
                             </div>
-                            <div className="mt-2 text-sm leading-4 tracking-wide text-white font-helvetica font-light">The Emirates College for Advanced Education (ECAE) has</div>
-                            <div className="mt-2.75 flex items-center gap-3">
-                                <figure className="h-7.5 w-7.5 rounded-full overflow-hidden">
+                            <div className="hidden lg:block mt-2 text-sm leading-4 tracking-wide text-white font-helvetica font-light">The Emirates College for Advanced Education (ECAE) has</div>
+                            <div className="mt-2 lg:mt-2.75 flex items-center gap-3">
+                                <figure className="hidden lg:block h-7.5 w-7.5 rounded-full overflow-hidden">
                                     <Image src="/images/blogs/Ellipse 5.png" alt="blog1" width={1000} height={1000} className="h-full w-full object-cover" />
                                 </figure>
                                 <ul className="flex items-center gap-1.5">
@@ -44,17 +46,19 @@ export default function Blog() {
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-2 grid-rows-2 gap-5">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-3 lg:grid-rows-2 gap-y-5 lg:gap-5">
                         <div className="max-w-157.5 w-full col-span-2 relative">
+                            <div className="absolute top-3 lg:top-5 right-3 lg:right-5 z-40 border border-primary/80 bg-primary/80 rounded-[6px] p-2 lg:p-2.5 capitalize text-xs lg:text-sm font-product-sans-medium font-light w-fit text-white">
+                                Corporate Events
+                            </div>
                             <figure className="h-68.5 max-w-157.5 w-full overflow-hidden">
                                 <Image src="/images/blogs/Group 48529sadf.png" alt="blog1" width={1000} height={1000} className="h-full w-full object-cover" />
                             </figure>
-                            <div className="absolute w-[94%] bottom-3.5 left-1/2 -translate-x-1/2 px-5 py-[14.67px] bg-black/20 backdrop-blur-lg z-10">
-                                <div className="border border-primary/80 bg-primary/80 rounded-[6px] p-2.5 capitalize font-product-sans-medium font-light w-fit text-white">Corporate Events</div>
-                                <div className="mt-1.75 text-[18px] leading-6.5 font-product-sans-bold font-medium text-white">
+                            <div className="absolute w-full bottom-0 left-0 px-5 py-[14.67px] blog-page-gradient z-10">
+                                <div className="text-sm lg:text-[18px] leading-4.5 lg:leading-6.5 font-product-sans-bold font-medium text-white">
                                     Emirates College for Advanced Education concludes 10th Annual Autism Day event
                                 </div>
-                                <div className="mt-2.75 flex items-center gap-3">
+                                <div className="mt-2 lg:mt-2.75 flex items-center gap-3">
                                     <ul className="flex items-center gap-1.5">
                                         <li className="">
                                             <p className="font-product-sans-medium font-normal text-white text-xs leading-3.5">Maximus Wooten</p>
@@ -72,13 +76,17 @@ export default function Blog() {
                             </div>
                         </div>
                         <div className="row-start-2 col-start-1 relative">
+                            <div className="absolute top-3 lg:top-5 right-3 lg:right-5 z-40 border border-primary/80 bg-primary/80 rounded-[6px] p-2 lg:p-2.5 capitalize text-xs lg:text-sm font-product-sans-medium font-light w-fit text-white">
+                                Activations
+                            </div>
                             <figure className="h-68.5 w-full overflow-hidden">
                                 <Image src="/images/blogs/Group 48524.png" alt="blog1" width={1000} height={1000} className="h-full w-full object-cover" />
                             </figure>
-                            <div className="absolute w-[94%] bottom-3.5 left-1/2 -translate-x-1/2 p-3.5 bg-black/20 backdrop-blur-lg z-10">
-                                <div className="border border-primary/80 bg-primary/80 rounded-[6px] p-2.5 capitalize font-product-sans-medium font-light w-fit text-white">Activations</div>
-                                <div className="mt-1.75 text-[16px] leading-5 font-product-sans-bold font-medium text-white">Emirates College for Advanced Education concludes...</div>
-                                <div className="mt-2.75 flex items-center gap-3">
+                            <div className="absolute w-full bottom-0 left-0 p-3.5 blog-page-gradient z-10">
+                                <div className="text-sm lg:text-[16px] leading-4.5 lg:leading-5 font-product-sans-bold font-medium text-white">
+                                    Emirates College for Advanced Education concludes...
+                                </div>
+                                <div className="mt-2 lg:mt-2.75 flex items-center gap-3">
                                     <ul className="flex items-center gap-1.5">
                                         <li className="">
                                             <p className="font-product-sans-medium font-normal text-white text-xs leading-3.5">Maximus Wooten</p>
@@ -95,14 +103,18 @@ export default function Blog() {
                                 </div>
                             </div>
                         </div>
-                        <div className="row-start-2 col-start-2 relative">
+                        <div className="row-start-3 lg:row-start-2 col-start-1 lg:col-start-2 relative">
+                            <div className="absolute top-3 lg:top-5 right-3 lg:right-5 z-40 border border-primary/80 bg-primary/80 rounded-[6px] p-2 lg:p-2.5 capitalize text-xs lg:text-sm font-product-sans-medium font-light w-fit text-white">
+                                experiental
+                            </div>
                             <figure className="h-68.5 w-full overflow-hidden">
                                 <Image src="/images/blogs/Group 48528.png" alt="blog1" width={1000} height={1000} className="h-full w-full object-cover" />
                             </figure>
-                            <div className="absolute w-[94%] bottom-3.5 left-1/2 -translate-x-1/2 p-3.5 bg-black/20 backdrop-blur-lg z-10">
-                                <div className="border border-primary/80 bg-primary/80 rounded-[6px] p-2.5 capitalize font-product-sans-medium font-light w-fit text-white">experiental</div>
-                                <div className="mt-1.75 text-[16px] leading-5 font-product-sans-bold font-medium text-white">Emirates College for Advanced Education concludes...</div>
-                                <div className="mt-2.75 flex items-center gap-3">
+                            <div className="absolute w-full bottom-0 left-0 p-3.5 blog-page-gradient z-10">
+                                <div className="text-sm lg:text-[16px] leading-4.5 lg:leading-5 font-product-sans-bold font-medium text-white">
+                                    Emirates College for Advanced Education concludes...
+                                </div>
+                                <div className="mt-2 lg:mt-2.75 flex items-center gap-3">
                                     <ul className="flex items-center gap-1.5">
                                         <li className="">
                                             <p className="font-product-sans-medium font-normal text-white text-xs leading-3.5">Maximus Wooten</p>
@@ -124,7 +136,7 @@ export default function Blog() {
             </div>
 
             <div className="mt-11.25">
-                <h2 className="font-helvetica font-bold text-[34px] leading-10 mb-4">Global Highlights</h2>
+                <h2 className="font-helvetica font-bold text-2xl lg:text-[34px] leading-7 lg:leading-10 mb-4">Global Highlights</h2>
                 <EmblaCarousel />
             </div>
         </div>
--- components/layout/Blog/BlogListCarouselCard.tsx
+++ components/layout/Blog/BlogListCarouselCard.tsx
@@ -111,15 +111,17 @@ export function EmblaCarousel() {
             <div className="overflow-hidden" ref={emblaRef}>
                 <div className="flex gap-1.75">
                     {CAROUSEL_DATA.map((item) => (
-                        <div key={item.id} className="flex-[0_0_calc((100%-14px)/3)] min-w-0 relative">
+                        <div key={item.id} className="flex-[0_0_calc(100%/1)] lg:flex-[0_0_calc((100%-14px)/3)] min-w-0 relative">
+                            <div className="absolute top-5 right-5 z-40 border border-primary/80 bg-primary/80 rounded-[6px] p-2.5 capitalize text-xs lg:text-sm font-product-sans-medium font-light w-fit text-white">
+                                {item.category}
+                            </div>
                             <figure className="h-80 w-full overflow-hidden">
                                 <Image src={item.image} alt={item.title} width={1000} height={1000} className="h-full w-full object-cover" />
                             </figure>
-                            <div className="absolute w-[94%] bottom-3.5 left-1/2 -translate-x-1/2 p-3.5 bg-black/20 backdrop-blur-lg z-10">
-                                <div className="border border-primary/80 bg-primary/80 rounded-[6px] p-2.5 capitalize font-product-sans-medium font-light w-fit text-white">{item.category}</div>
-                                <div className="mt-1.75 text-[16px] leading-5 font-product-sans-bold font-extralight text-white">{item.title}</div>
+                            <div className="absolute w-full bottom-0 left-0 p-3.5 blog-page-gradient z-10">
+                                <div className="text-sm lg:text-[16px] leading-4.5 lg:leading-5 font-product-sans-bold font-medium text-white">{item.title}</div>
                                 {/* <div className="mt-2 text-sm leading-4 tracking-wide text-white font-helvetica font-light">The Emirates College for Advanced Education (ECAE) has</div> */}
-                                <div className="mt-2.75 flex items-center gap-3">
+                                <div className="mt-2 lg:mt-2.75 flex items-center gap-3">
                                     {/* <figure className="h-7.5 w-7.5 rounded-full overflow-hidden">
                                                                 <Image src="/images/blogs/Ellipse 5.png" alt="blog1" width={1000} height={1000} className="h-full w-full object-cover" />
                                                             </figure> */}
@@ -144,7 +146,7 @@ export function EmblaCarousel() {
             </div>
 
             {/* Navigation Buttons */}
-            <div className="absolute top-1/2 -translate-y-1/2 -left-5 w-[104%] h-fit flex items-center justify-between gap-4">
+            <div className="absolute top-1/2 -translate-y-1/2 -left-5 w-[111%] lg:w-[104%] h-fit flex items-center justify-between gap-4">
                 <button
                     onClick={scrollPrev}
                     disabled={prevBtnDisabled}
